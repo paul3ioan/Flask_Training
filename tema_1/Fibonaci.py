33ebe58add7af31573129a1e563c05c1a6ab321a
paul3ioan <paul3ioan@gmail.com>
2021-03-18 21:40:01 +0200

    asta e tema mea frate

---
+++ tema_1/Fibonaci.py
@@ -0,0 +1,11 @@
+input = int(input())
+list = [1,1]
+# test is i need only the first element of Fibonnaci
+if input == 1:
+    print(1)
+    exit(0)
+# create Fibonnaci list untill I have n elements
+while len(list) is not input:
+    list.append(list[len(list) - 1] + list[len(list)- 2])
+# just print
+for i in list:
---
+++ tema_1/egale.py
@@ -0,0 +1,21 @@
+filein = open("egale.in", 'r')
+fileout = open("egale.out", 'w')
+input = [int(i) for i in filein.read().split()]
+#input[0] = task
+list = []
+for i in range(1, 100):
+    for j in range(ord("1"),ord("9") + 1):
+        list.append(int(chr(j) * i))
+# list of good elements
+if input[0] == 1:
+    for i in list:
+        if i >= input[1] and i <=input[2]:
+            fileout.write(str(i) + " ")
+    #task1 if my element is in interval I write it
+else:
+    lim = int(input[1] * '9')
+    for i in list:
+        if i > lim:
+            break
+        fileout.write(str(i)+" ")
+    #if my element is bigger than lim I stop
